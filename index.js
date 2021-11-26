870fa2ad681cca6b7653e8c1145b91071ff78338
destiny2notifs <65279644+destiny2notifs@users.noreply.github.com>
2021-11-26 09:06:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+const Url='https://www.bungie.net/platform/Destiny2/Stats/Definition/';
+$('.btn').click(function(){
+    $.get(Url, function(data, status){
+      console.log('${data}')
+    });
+})
